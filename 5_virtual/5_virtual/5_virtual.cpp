24cb9dd53b113af29481686410f48d9c402c6b56
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-02-24 23:54:56 +0300

    add: virtual, functions pointers

--- 5_virtual/5_virtual/5_virtual.cpp
+++ 5_virtual/5_virtual/5_virtual.cpp
@@ -104,7 +104,6 @@
 
 
 // === objects array
-
 //class Unit
 //{
 //private:
@@ -363,48 +362,401 @@
 
 
 // ===== ковариантный тип возврата
+//class Parent
+//{
+//public:
+//	void render()
+//	{
+//		std::cout << "Parent::render()\n";
+//	}
+//	virtual Parent* get()
+//	{
+//		return this;
+//	}
+//};
+//
+//
+//class Child : public Parent
+//{
+//public:
+//	void render()
+//	{
+//		std::cout << "Child::render()\n";
+//	}
+//
+//	virtual Child* get() override				// <-- ковариантный тип возврата
+//	{
+//		return this;
+//	}
+//};
+//
+//int main()
+//{
+//	Child c;
+//
+//	Parent* ptr = &c;
+//
+//	c.get()->render();
+//
+//	ptr->get()->render();
+//
+//
+//
+//	return 0;
+//}
 
-class Parent
+
+
+
+
+// ===== virtual destructor !!!!
+// 
+//class Parent
+//{
+//public:
+//	virtual ~Parent()
+//	{
+//		std::cout << "Calling ~Parent()\n";
+//	}
+//};
+//
+//
+//class Child : public Parent
+//{
+//private:
+//	int count;
+//	int* arr;
+//
+//public:
+//	Child(int count):
+//		count{count},
+//		arr{new int[count]}
+//	{}
+//
+//	virtual ~Child() override
+//	{
+//		if (this->arr != nullptr)
+//			delete[] this->arr;
+//
+//		std::cout << "Calling ~Child()\n";
+//	}
+//};
+//
+//int main()
+//{
+//	/*Child* c = new Child{ 10 };
+//	delete c;*/
+//
+//	Parent* p = new Child{ 10 };
+//	delete p;
+//
+//	return 0;
+//}
+
+
+
+
+
+// === Раннее (статическои) и позднее (динамическое) связывание
+
+
+// --- Раннее (статическои) связывание
+
+//int sum(int a, int b) { return a + b; }
+//int sub(int a, int b) { return a - b; }
+//int mul(int a, int b) { return a * b; }
+//
+//int main()
+//{
+//	int a;
+//	std::cout << "Enter a: ";
+//	std::cin >> a;
+//
+//	int b;
+//	std::cout << "Enter b: ";
+//	std::cin >> b;
+//
+//	char op;
+//	std::cout << "Enter op: ";
+//	std::cin >> op;
+//
+//	int res{};
+//
+//	switch (op) 
+//	{
+//	case '+':
+//		res = sum(a, b);
+//		break;
+//	case '-':
+//		res = sub(a, b);
+//		break;
+//	case '*':
+//		res = mul(a, b);
+//		break;
+//	}
+//
+//	std::cout << res;
+//}
+
+
+// --- Указатели на ф-цию
+
+
+//int f1()
+//{
+//	return 34;
+//}
+//
+//int f2()
+//{
+//	return 17;
+//}
+//
+//int f3(int a)
+//{
+//	return 56;
+//}
+//
+//
+//int main()
+//{
+//	/*std::cout << f1 << '\n';
+//
+//	int(*fPtr)() = f1;
+//
+//	std::cout << fPtr;*/
+//
+//
+//	int(*fptr)() = nullptr;
+//	fptr = f1;
+//	fptr = f2;
+//	// fptr = f3;			// ERROR
+//
+//	std::cout << (*fptr)() << '\n';
+//	std::cout << fptr() << '\n';
+//
+//
+//	return 0;
+//}
+
+
+
+// --- позднее связывание
+//int sum(int a, int b) { return a + b; }
+//int sub(int a, int b) { return a - b; }
+//int mul(int a, int b) { return a * b; }
+//
+//int main()
+//{
+//	int a;
+//	std::cout << "Enter a: ";
+//	std::cin >> a;
+//
+//	int b;
+//	std::cout << "Enter b: ";
+//	std::cin >> b;
+//
+//	char op;
+//	std::cout << "Enter op: ";
+//	std::cin >> op;
+//
+//	int(*ptr)(int, int) = nullptr;
+//
+//	switch (op) 
+//	{
+//	case '+':
+//		ptr = sum;
+//		break;
+//	case '-':
+//		ptr = sub;
+//		break;
+//	case '*':
+//		ptr = mul;
+//		break;
+//	}
+//
+//	int res{};
+//	if (ptr != nullptr)
+//		res = ptr(a, b);					// <--- позднее связывание
+//
+//	std::cout << res;
+//}
+
+
+// TASK
+// Написать ф-цию, суммирующую четные элементы массива
+
+//int sum(int* arr, int count)
+//{
+//	int res{};
+//
+//	for (int i{}; i < count; ++i)
+//	{
+//		if (arr[i] % 2 == 0)
+//			res += arr[i];
+//	}
+//
+//	return res;
+//}
+
+
+//int sum(int* arr, int count, bool(*predicate)(int))
+//{
+//	int res{};
+//
+//	for (int i{}; i < count; ++i)
+//	{
+//		if (predicate(arr[i]))
+//			res += arr[i];
+//	}
+//
+//	return res;
+//}
+//
+//
+//
+//bool isEven(int n) { return n % 2 == 0; }
+//bool isDivThree(int n) { return n % 3 == 0; }
+//
+//int main()
+//{
+//	int* arr = new int[6] {5, 6, 7, 8, 9, 2};
+//	//std::cout << sum(arr, 6, isEven);
+//	std::cout << sum(arr, 6, isDivThree);
+//
+//	
+//
+//	return 0;
+//}
+
+
+
+
+// ---- лямбды
+// []()->type {}
+
+
+//void(*fptr)() = []() {std::cout << "Vasisa"; };
+//
+//void(*fptr2)()
+//{
+//	[]() 
+//	{
+//		std::cout << "Vasisa"; 
+//	}
+//};
+//
+//
+//
+//bool(*fptr3)(int) = [](int n) {return n % 2 == 0; };
+
+
+
+
+//int sum(int* arr, int count, bool(*predicate)(int))
+//{
+//	int res{};
+//
+//	for (int i{}; i < count; ++i)
+//	{
+//		if (predicate(arr[i]))
+//			res += arr[i];
+//	}
+//
+//	return res;
+//}
+//
+//
+//int main()
+//{
+//	int* arr = new int[6] {5, 6, 7, 8, 9, 2};
+//	
+//
+//	int result1 = sum(arr, 6, [](int n) {return n % 2 == 0; });
+//	int result2 = sum(arr, 6, [](int n) {return n % 2 > 0; });
+//	int result3 = sum(arr, 6, [](int n) {return n > 0; });
+//
+//	
+//
+//	return 0;
+//}
+
+
+
+
+
+// EXAMPLE
+#include <vector>
+#include <format>
+
+typedef void (*handler)(std::string);
+
+
+
+void sendToTelegram(std::string message)
 {
+	std::cout << "Telegram: " << message << '\n';
+}
+
+void sendSMS(std::string message)
+{
+	std::cout << "SMS: " << message << '\n';
+}
+
+void sendEmail(std::string message)
+{
+	std::cout << "Email: " << message << '\n';
+}
+
+
+
+class Account
+{
+private:
+	int id;
+	int sum;
+	std::vector<handler> depositHandlers;
+
 public:
-	void render()
+	Account(int id, int sum):
+		id{id},
+		sum{sum}
+	{}
+
+	void addDepositHandler(handler h)
 	{
-		std::cout << "Parent::render()\n";
+		depositHandlers.push_back(h);
 	}
-	virtual Parent* get()
+
+	void deposit(int sum)
 	{
-		return this;
+		this->sum += sum;
+
+		
+		std::string message = "message from account";
+
+		for (int i = 0; i < depositHandlers.size(); ++i)
+			depositHandlers[i](message);
+	}
+
+	void withdraw(int sum)
+	{
+
 	}
 };
 
-
-class Child : public Parent
-{
-public:
-	void render()
-	{
-		std::cout << "Child::render()\n";
-	}
-
-	virtual Child* get() override				// <-- ковариантный тип возврата
-	{
-		return this;
-	}
-};
 
 int main()
 {
-	Child c;
-
-	Parent* ptr = &c;
-
-	c.get()->render();
-
-	ptr->get()->render();
+	Account acc{ 102, 1000 };
+	acc.addDepositHandler(sendSMS);
+	acc.addDepositHandler(sendEmail);
 
 
+	acc.deposit(100);
+	acc.deposit(100);
+	acc.deposit(100);
 
-	return 0;
 }
 
 
